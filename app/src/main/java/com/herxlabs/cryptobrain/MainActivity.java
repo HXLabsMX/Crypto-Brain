d38eb3413298ed27c15167e807a4f3021fa75b37
Heriberto Arias Torres <er.mx123@gmail.com>
2017-04-18 22:41:00 -0500

    Se creo Activity "Acerca de"
    Se creo un Activity sencillo para mostrar información del proyecto

--- app/src/main/java/com/herxlabs/cryptobrain/MainActivity.java
+++ app/src/main/java/com/herxlabs/cryptobrain/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
@@ -86,5 +88,26 @@ public class MainActivity extends AppCompatActivity {
             Toast toastMessage = Toast.makeText(context, "Nothing to share", Toast.LENGTH_SHORT);
             toastMessage.show();
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
